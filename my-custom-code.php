9a427aa7e1a4093d7cc64f9b5fabf29c4db75bf2
Eric <ericbusch@gmail.com>
2021-09-01 10:53:47 -0400

    Update my-custom-code.php

--- my-custom-code.php
+++ my-custom-code.php
@@ -29,3 +29,4 @@ require_once 'prune-action-scheduler-actions-more-often.php';
 //require_once 'sort-products-from-specific-merchants-last.php';
 //require_once 'add-size-attribute-for-each-product.php';
 //require_once 'limit-results-returned-by-comparison-set.php';
+
